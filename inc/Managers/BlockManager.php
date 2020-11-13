57f15539a364dc06bf13febe0460331058c722ad
Aran Wilcox <aran.wilcox@affinity-digital.com>
2020-11-13 16:50:12 +0000

    updating the block manager to include correct content block registers

--- inc/Managers/BlockManager.php
+++ inc/Managers/BlockManager.php
@@ -17,12 +17,12 @@ class BlockManager extends BaseController{
 
 	public function register() : void {
 		add_action( 'init', [ $this, 'co_committee_table_columns_block_register' ] );
-		add_action( 'init', [ $this, 'co_capability_degraded_row_columns_block_register' ] );
+		add_action( 'init', [ $this, 'co_committee_table_row_columns_block_register' ] );
 		add_action( 'init', [ $this, 'co_event_date_time_register' ] );
 	}
 
 	// START: Register - Purpose content block
-	public function co_committee_table_columns_columns_block_register(): void {
+	public function co_committee_table_columns_block_register(): void {
 
 		wp_register_script(
 			'co_committee_table_columns_columns_block_editor_script',
@@ -40,10 +40,10 @@ class BlockManager extends BaseController{
 	}  // END: Register - Purpose content block
 
 	// START: Register - Intro content block
-	public function co_capability_degraded_row_block_register(): void {
+	public function co_committee_table_row_columns_block_register(): void {
 
 		wp_register_script(
-			'co_capability_degraded_row_block_editor_script',
+			'co_committee_table_row_columns_block_editor_script',
 			$this->plugin_url . 'dist/editor-output.js',
 			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components','wp-edit-post', 'wp-plugins' )
 		);
@@ -51,7 +51,7 @@ class BlockManager extends BaseController{
 		register_block_type(
 			'co/committee-table-row',
 			array(
-				'editor_script'   => 'co_capability_degraded_row_block_editor_script'
+				'editor_script'   => 'co_committee_table_row_columns_block_editor_script'
 			)
 		);
 
